ac301d1049f77a271d6f2ad29b0ce24cfe4bfdbd
Tim-orius <tim.rosenkranz1@t-online.de>
2021-04-28 20:57:20 +0200

    Improvements to warning system

--- mr_bottinger.py
+++ mr_bottinger.py
@@ -11,7 +11,7 @@ global log_id
 global pasted_logs
 
 async def extract_content(content):
-    """ """
+    """Extract content from log files"""
     extracted = []
     error_msg = []
     version = "0.0.0.0"
@@ -29,8 +29,8 @@ async def extract_content(content):
     return version, extracted, error_msg
 
 
-async def check_user(user: str = ""):
-    """ """
+async def warn_user(user: str = ""):
+    """Check for existence of the user id in the warning files"""
 
     with open("warns/warn2.txt", "r+") as w2:
         if user in w2.read():
@@ -45,16 +45,28 @@ async def check_user(user: str = ""):
                     return 1
 
 
-async def warning_handler(message):
-    case = await check_user(str(message.author.id))
-    if case == 3:
-        await message.guild.ban(message.author, reason="3 Warnings")
+async def warn_remove(user_id: str, file: int):
+    """Remove warnings from a user"""
 
-    return case
+    if file != 1 and file != 2:
+        return False
+    
+    found = False
+    with open("warns/warn"+str(file)+".txt", "r") as f:
+        lines = f.readlines()
+
+    if user_id in lines:
+        found = True
+        lines.remove(user_id)
+
+    with open("warns/warn"+str(file)+".txt", "w") as f:
+        f.writelines(lines)
+
+    return found
 
 
 def main():
-    """ """
+    """Main method to start bot"""
 
     global log_id
     global pasted_logs
@@ -89,17 +101,18 @@ def main():
     # Message event
     @client.event
     async def on_message(message):
-
+        """On sent messages"""
         global log_id
 
         if message.author == client.user:# or message.author.id in vip_users:
             return
 
-        if any(word in str(message.content) for word in bad_words) and filter:
-            warn = await warning_handler(message)
+        if any(word in str(message.content) for word in bad_words) and filter_chat:
+            warn = await warn_user(str(message.author.id))
             if warn < 3:
                 await message.reply("Your message contains inappropirate wording. Warnings: " + str(warn))
             else:
+                await message.guild.ban(message.author, reason="3 Warnings")
                 await message.reply("User banned.")
             await message.delete()
             return
@@ -145,10 +158,11 @@ def main():
                         log_id += 1
 
                 elif not 'image' in content_type.lower() or "video" in content_type.lower():
-                    warn = await warning_handler(message)
+                    warn = await warn_user(str(message.author.id))
                     if warn < 3:
                         await message.reply("The file type '" + content_type + "' is not allowed. Warnings: " + str(warn))
                     else:
+                        await message.guild.ban(message.author, reason="3 Warnings")
                         await message.reply("User banned.")
                     await message.delete()
                     break
@@ -164,11 +178,12 @@ def main():
                 await message.reply("Your report has been saved.")
 
 
-
+    # Ping command
     @slash.slash(name="ping", description="Show your ping.", guild_ids=guild_ids)
     async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
         await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
+    # Upload command
     @slash.slash(name="upload", description="Upload a log to pastebin.",
                  options=[create_option(name="logid", description="The log ID of the log that should be uploaded.",
                           option_type=3,required=True)], guild_ids=guild_ids)
@@ -182,6 +197,7 @@ def main():
         else:
             await ctx.send(content="The specified log ID is invalid.")
 
+    # Filter command
     @slash.slash(name="filter", description="Set chat filtering",
                  options=[create_option(name="state", description="True / False",
                                         option_type=3, required=True)], guild_ids=guild_ids)
@@ -189,8 +205,43 @@ def main():
         filter_chat = True if state == "True" else False
         await ctx.send("Chat filtering and warning system is now " + ("enabled" if filter_chat else "disabled"))
 
+    # Unwarn command
+    @slash.slash(name="unwarn", description="Remove warnings from user",
+                 options=[create_option(name="user", description="Name of the user",
+                                        option_type=6, required=True),
+                          create_option(name="amount", description="Amount of warnings to remove (max. 2 possible)",
+                                        option_type=4, required=True)], guild_ids=guild_ids)
+    async def _unwarn(ctx, user: discord.User, amount: int):
+        found = await warn_remove(str(user.id), 2)
+        if amount == 1:
+            if not found:
+                found = await warn_remove(str(user.id), 1)
+                if not found:
+                    await ctx.send("Removed 0 warnings from user "+str(user)+". Warnings left: 0.")
+                else:
+                    await ctx.send("Removed 1 warning from user "+str(user)+". Warnings left: 0.")
+            else:
+                await ctx.send("Removed 1 warning from user "+str(user)+". Warnings left: 1.")
+        elif amount > 1:
+            await warn_remove(str(user.id), 1)
+            await ctx.send("Removed all warnings from user "+str(user)+". Warnings left: 0.")
 
+    # Warn command
+    @slash.slash(name="warn", description="Warn a user",
+                 options=[create_option(name="user", description="Name of the user",
+                                        option_type=6, required=True),
+                          create_option(name="reason", description="Reason for the warning",
+                                        option_type=3, required=False)], guild_ids=guild_ids)
+    async def warn(ctx, user: discord.Member, reason: str = "None"):
+        user_id = user.id
+        warn = await warn_user(str(user_id))
+        if warn < 3:
+            await ctx.send("User " + str(user) + " has been warned. Reason: " + reason + "; Warnings: " + str(warn))
+        else:
+            await ctx.guild.ban(user, reason="3 Warnings")
+            await ctx.reply("User banned due to third warning.")
 
+    # Run bot
     client.run(os.getenv('TOKEN'))
 
 
